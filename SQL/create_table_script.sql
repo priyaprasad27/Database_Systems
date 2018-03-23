a527da3da8dc2be64c032d740ed440eaded7a32f
Peter <peter.j.martinson@gmail.com>
2018-03-23 16:59:51 -0400

    Add all the key constraints

--- SQL/create_table_script.sql
+++ SQL/create_table_script.sql
@@ -21,26 +21,26 @@ CREATE TABLE TEACHER (
 create table SUBJECT (
   subject_id integer(10),
   subject_name varchar(30),
-  teacher_id integer(6),
+  fk_teacher_id integer(6),
   constraint SUBJECT_PK primary key (SUBJECT_ID),
-  constraint TEACHER_FK foreign key (TEACHER_ID) references TEACHER(TEACHER_ID)
+  constraint TEACHER_FK foreign key (FK_TEACHER_ID) references TEACHER(TEACHER_ID)
 );
 
 CREATE TABLE HOMEROOM (
   HOMEROOM_ID integer(6),
   HOMEROOM_NAME varchar(30),
-  TEACHER_ID integer(6),
+  FK_TEACHER_ID integer(6),
   constraint HOMEROOM_PK primary key (HOMEROOM_ID),
-  constraint TEACHER_FK foreign key(TEACHER_ID) references TEACHER(TEACHER_ID)
+  constraint TEACHER_FK foreign key(FK_TEACHER_ID) references TEACHER(TEACHER_ID)
 );
 
 CREATE TABLE ASSIGNMENT (
   ASSIGNMENT_ID integer(6),
   ASSIGNEMENT_NAME varchar(30),
   URL varchar(30),
-  SUBJECT_ID integer(10),
-  CONSTRAINT ASSIGNMENT_PK PRIMARY KEY(ASSIGNMENT_ID),
-  CONSTRAINT SUBJECT_FK FOREIGN KEY(SUBJECT_ID) REFERENCES SUBJECT(SUBJECT_ID)
+  FK_SUBJECT_ID integer(10),
+  constraint ASSIGNMENT_PK primary key(ASSIGNMENT_ID),
+  constraint SUBJECT_FK foreign key(FK_SUBJECT_ID) references SUBJECT(SUBJECT_ID)
 );
 
 CREATE TABLE STUDENT(
@@ -48,9 +48,9 @@ CREATE TABLE STUDENT(
   FNAME varchar(20),
   LNAME varchar(20),
   BIRTHDATE DATE,
-  HOMEROOM_ID integer(6),
-  CONSTRAINT STUDENT_PK PRIMARY KEY(STUDENT_ID),
-  CONSTRAINT ROOM_fk foreign key(homeroom_id) REFERENCES homeroom(homeroom_id)
+  FK_HOMEROOM_ID integer(6),
+  constraint STUDENT_PK primary key(STUDENT_ID),
+  constraint ROOM_fk foreign key(FK_HOMEROOM_ID) references HOMEROOM(HOMEROOM_ID)
 );
 
 
@@ -58,16 +58,20 @@ CREATE TABLE HOMEWORK (
   HOMEWORK_ID integer(6),
   URL varchar(30),
   FINAL_GRADE VARCHAR(2),
-  STUDENT_ID integer(6),
-  ASSIGNMENT_ID integer(6),
-  constraint homework_pk primary key(homework_id), constraint student_fk foreign key(student_id) references student(student_id)
+  FK_STUDENT_ID integer(6),
+  FK_ASSIGNMENT_ID integer(6),
+  constraint HOMEWORK_PK primary key(HOMEWORK_ID),
+  constraint STUDENT_FK foreign key(FK_STUDENT_ID) references STUDENT(STUDENT_ID),
+  constraint ASSIGNMENT_FK foreign key(FK_ASSIGNMENT_ID) references ASSIGNMENT(ASSIGNMENT_ID)
 );
 
 CREATE TABLE TAKEN_BY (
-  STUDENT_ID integer(6),
-  SUBJECT_ID integer(6),
+  FK_STUDENT_ID integer(6),
+  FK_SUBJECT_ID integer(6),
   FINAL_GRADE VARCHAR(2),
-  constraint taken_pk primary key(student_id,subject_id)
+  constraint TAKEN_PK primary key(FK_STUDENT_ID,FK_SUBJECT_ID),
+  constraint STUDENT_FK foreign key(FK_STUDENT_ID) references STUDENT(student_id),
+  constraint SUBJECT_FK foreign key(FK_SUBJECT_ID) references SUBJECT(SUBJECT_ID)
 );
 
 
