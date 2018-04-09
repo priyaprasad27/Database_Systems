51770e8b0592142982f00593391851f9decc399f
Peter Martinson <peter.j.martinson@gmail.com>
2018-04-08 21:03:35 -0400

    Normalize the cases
    SQL commands should be <UPPER CASE>
    Object names should be <lower case>

--- SQL/NewSchool_Operations_script.sql
+++ SQL/NewSchool_Operations_script.sql
@@ -3,46 +3,46 @@ Each query has the operation commented and sample data used in the operations
 A separate document with results will be attached***********************/
 
 -- Teachers can add a grade to the Student Work. We need to add conditions for both assignment_id and student_id
-UPDATE HOMEWORK
-SET FINAL_GRADE = 'A-'
-WHERE FK_ASSIGNMENT_ID = 1
+UPDATE homework
+SET final_grade = 'A-'
+WHERE fk_assignment_id = 1
 and fk_student_id = 30;
 
-
-/*Transfer a student from one homeroom to another*/
-UPDATE STUDENT 
-SET FK_HOMEROOM_ID = 2
+-- Transfer a student from one homeroom to another
+UPDATE student 
+SET fk_homeroom_id = 2
 where student_id = 1;
 
 --Display a list of student assignments for a subject.Sample subject id = 1 
-SELECT S.SUBJECT_NAME, g.ASSIGNMENT_ID, g.ASSIGNMENT_NAME, g.URL 
-FROM ASSIGNMENT G, SUBJECT S
-WHERE S.SUBJECT_ID = G.FK_SUBJECT_ID
-and g.fk_subject_id=1;
+SELECT s.subject_name, g.assignment_id, g.assignment_name, g.url 
+FROM assignment g, subject s
+WHERE s.subject_id = g.fk_subject_id
+AND s.subject_name = 'Astronomy';
+-- and g.fk_subject_id=1;
 
 -- Display a list of all students taught by a given teacher. Sample teacher - Teacher id 2, Carolyn Porco
-SELECT  T.First_name||' '||t.last_Name as teacher_Name, S.FNAME||' '||s.LName as student_name
-FROM STUDENT S, HOMEROOM H, teacher t
-WHERE S.FK_HOMEROOM_ID = H.HOMEROOM_ID
-AND H.FK_TEACHER_ID = T.TEACHER_ID
-and t.teacher_id = 2;
+SELECT  t.first_name||' '||t.last_Name as teacher_Name, s.fname||' '||s.lname as student_name
+FROM student s, homeroom h, teacher t
+WHERE s.fk_homeroom_id = h.homeroom_id
+AND h.fk_teacher_id = t.teacher_id
+AND t.teacher_id = 2;
 
 -- DISPLAY A LIST OF ALL THE STUDENT WORK BY A STUDENT. Sample student_id = 22
-SELECT  S.FNAME||' '||S.LNAME as student_Name, H.URL as homework_URL
-FROM HOMEWORK H, STUDENT S
-WHERE S.STUDENT_ID = H.FK_STUDENT_ID
-and S.STUDENT_ID = 22;
+SELECT  s.fname||' '||s.lname as student_Name, h.url as homework_url
+FROM homework h, student s
+WHERE s.student_id = h.fk_student_id
+AND s.student_id = 22;
 
 -- Display a list of student work for a classroom. Sample homeroom_id = 2
-SELECT h.FK_STUDENT_ID AS HOMEROOM_2_STUDENTS, s.fname||' '||s.lname as Student_Name, h.URL AS STUDENT_WORK
-FROM HOMEWORK h, STUDENT S
-WHERE FK_STUDENT_ID IN ( SELECT STUDENT_ID FROM STUDENT WHERE FK_HOMEROOM_ID = 2)
-and s.student_id = h.fk_student_id;
+SELECT h.fk_student_id AS homeroom_2_students, s.fname||' '||s.lname as student_name, h.url AS student_work
+FROM homework h, student s
+WHERE fk_student_id IN ( SELECT student_id FROM student WHERE fk_homeroom_id = 2)
+AND s.student_id = h.fk_student_id;
 
 
 -- INSERT A NEW STUDENT INTO THE DB. inserting 2 new students
-INSERT INTO STUDENT (STUDENT_ID,  FNAME,  LNAME,  BIRTHDATE,  FK_HOMEROOM_ID) VALUES (31 ,   'Priya',        'Prasad',        '03-May-1987',  3);
-INSERT INTO STUDENT (STUDENT_ID,  FNAME,  LNAME,  BIRTHDATE,  FK_HOMEROOM_ID) VALUES (32 ,   'Peter',        'Martinson',     '03-Jun-1987',  2);
+INSERT INTO student (student_id,  fname,  lname,  birthdate,  fk_homeroom_id) VALUES (31 ,   'Priya',        'Prasad',        '03-May-1987',  3);
+INSERT INTO student (student_id,  fname,  lname,  birthdate,  fk_homeroom_id) VALUES (32 ,   'Peter',        'Martinson',     '03-Jun-1987',  2);
 
 
 
